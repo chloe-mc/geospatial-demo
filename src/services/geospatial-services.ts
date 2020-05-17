05784a0664c199932861e23e7c8e6509eb794133
Chloe McKnight <cmcknight@bluebeam.com>
2020-05-17 16:00:26 -0500

    add goGolfing functionality

--- src/services/geospatial-services.ts
+++ src/services/geospatial-services.ts
@@ -6,3 +6,20 @@ export const generateWalkingPath = (
   const coordinates = turf.coordAll(holes);
   return turf.lineString(coordinates);
 };
+
+export const goGolfing = (
+  walkingPath: turf.Feature<turf.LineString>,
+  updatePosition: (newPosition: any) => void
+) => {
+  const pathCoordiantes = walkingPath.geometry?.coordinates;
+  let counter = 0;
+  const animate = () => {
+    if (counter === pathCoordiantes?.length) return;
+    const nextPoint = pathCoordiantes && turf.point(pathCoordiantes[counter]);
+    updatePosition(nextPoint);
+    requestAnimationFrame(animate);
+    counter += 1;
+  };
+
+  animate();
+};
